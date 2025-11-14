a524ffb4784bdbb8588883f527646d62be1a40e9
Kim-si-hyeon <ksh402@hs.ac.kr>
2025-11-14 14:32:40 +0900

    Delete workout directory
    .

--- workout/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import RunningSession
-
-class RunningSessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RunningSession
-        fields = '__all__'
