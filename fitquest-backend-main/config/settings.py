fcfda1bcd4fb35c2a099d7d699fb18c29c309bc4
fswjfhwl <fswjfhwl@hs.ac.kr>
2026-01-14 21:59:53 +0900

    Update settings.py

--- fitquest-backend-main/config/settings.py
+++ fitquest-backend-main/config/settings.py
@@ -49,12 +49,16 @@ ROOT_URLCONF = "config.urls"
 WSGI_APPLICATION = "config.wsgi.application"
 
 # --------------------------------------------------
-# 4. 데이터베이스 (SQLite3)
+# 4. 데이터베이스 (MySQL)
 # --------------------------------------------------
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '3306',
     }
 }
 
