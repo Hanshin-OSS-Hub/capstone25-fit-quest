c3ea39711fb701064cb1c6d7d4295ca9b7981559
Kim-si-hyeon <ksh402@hs.ac.kr>
2026-03-30 10:24:22 +0900

    Delete BackEnd directory

--- BackEnd/fitquest/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class FitquestConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fitquest'
-
-    def ready(self):
-        import fitquest.signals
--- BackEnd/fitquest/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import CustomUser, UserSettings, UserStreak
-
-@receiver(post_save, sender=CustomUser)
-def create_related(sender, instance, created, **kwargs):
-    if created:
-        UserSettings.objects.get_or_create(user=instance)
-        UserStreak.objects.get_or_create(user=instance)
