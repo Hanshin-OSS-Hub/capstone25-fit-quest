71edca1a7bd132a6bfce1782ee1d5626914d61ce
fswjfhwl <fswjfhwl@hs.ac.kr>
2026-04-10 19:05:43 +0900

    Update deploy.sh

--- Infra/deploy.sh
+++ Infra/deploy.sh
@@ -3,29 +3,41 @@ set -e
 
 PROJECT_DIR=/home/ubuntu/capstone25-fit-quest
 BRANCH=main
+INFRA_DIR=$PROJECT_DIR/Infra
+
+echo "===== DEPLOY START ====="
 
 echo "[1] move to project dir"
-cd $PROJECT_DIR
+cd "$PROJECT_DIR"
 
-echo "[2] fetch latest info"
+echo "[2] fetch latest code from origin"
 git fetch origin
 
 echo "[3] checkout branch"
-git checkout $BRANCH
+git checkout "$BRANCH"
 
-echo "[4] pull latest code"
-git pull origin $BRANCH
+echo "[4] force sync local branch with origin/$BRANCH"
+git reset --hard "origin/$BRANCH"
 
-echo "[5] move to infra dir"
-cd $PROJECT_DIR/Infra
+echo "[5] remove untracked files"
+git clean -fd
 
-echo "[6] stop old containers"
+echo "[6] move to infra dir"
+cd "$INFRA_DIR"
+
+echo "[7] stop old containers"
 docker compose down
 
-echo "[7] rebuild containers"
-docker compose build --no-cache
+echo "[8] rebuild containers"
+docker compose build
 
-echo "[8] start containers"
+echo "[9] start containers"
 docker compose up -d
 
-echo "[9] done"
+echo "[10] cleanup unused docker images"
+docker image prune -af
+
+echo "[11] cleanup docker build cache"
+docker builder prune -af
+
+echo "===== DEPLOY SUCCESS ====="
